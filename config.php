032cbea9db87c946be6899831f2c811133aa53b2
Monique Kelly <munichkdev@gmail.com>
2022-05-31 17:42:36 -0300

    atualizado

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+    $dbHost = 'localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'cadastro_gocar';
+
+    $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+    //if ($conexao->connect_errno) {
+    //    echo "Erro";
+    //} else {
+    //    echo "Conexão efetuada com sucesso";
+    //};
+?>
